42e8738a13f0c92abb2c344d6d2389b7829d03e4
wsnlance <52989577+wsnlance@users.noreply.github.com>
2021-07-25 12:25:03 +0800

    work

--- src/main/java/io/github/wsnlance/oregenultimatex/OreGen.java
+++ src/main/java/io/github/wsnlance/oregenultimatex/OreGen.java
@@ -1,23 +1,50 @@
 package io.github.wsnlance.oregenultimatex;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Random;
+
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.plugin.java.JavaPlugin;
+//import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class OreGen extends BukkitRunnable {
 
-    private final JavaPlugin plugin;
+    //private final JavaPlugin plugin;
     private final Block b;
+    private final Map<String, Double> levelInfo;
+    
 
-    public OreGen(JavaPlugin plugin, Block b) {
-        this.plugin = plugin;
+    public OreGen(Block b, HashMap<String, Double> levelInfo) {
+        //this.plugin = plugin;
         this.b = b;
+        this.levelInfo = levelInfo;
     }
 
     @Override
     public void run() {
-    	plugin.getLogger().info(b.getType().toString());
-        b.setType(Material.SAND);
+    	//测试是否对圆石或石头生成
+    	//待做
+    	try {
+    		Random r = new Random();
+        	int i = r.nextInt(10000) + 1;
+        	Iterator<Entry<String, Double>> iter = levelInfo.entrySet().iterator();
+        	while(iter.hasNext()) {
+        		Map.Entry<String, Double> entry = (Map.Entry<String, Double>) iter.next();
+        		double prob = entry.getValue();
+        		//to be changed
+        		i -= prob;
+        		if (i <= 0) {
+        			String materialType = entry.getKey();
+        			b.setType(Material.valueOf(materialType));
+        			return;
+        		}
+        	}
+    	}catch (Exception e) {
+    		e.printStackTrace();
+    	}
     }
 }
--- src/main/java/io/github/wsnlance/oregenultimatex/OreGenUltimateX.java
+++ src/main/java/io/github/wsnlance/oregenultimatex/OreGenUltimateX.java
@@ -1,40 +1,217 @@
 package io.github.wsnlance.oregenultimatex;
 
-import org.bukkit.block.Block;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.HashMap;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockFormEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 
+//import org.black_ixx.bossshop.BossShop;
+//import org.black_ixx.bossshop.api.BossShopAPI;
+
+//import com.alibaba.fastjson.JSON;
+import com.google.gson.Gson;
+
+import net.milkbowl.vault.economy.Economy;
+
 public final class OreGenUltimateX extends JavaPlugin implements Listener{
-	@Override  //’‚¿Ô «javaµƒ“ª÷÷◊¢Ω‚£¨”√¿¥ºÏ≤‚œ¬√ÊonEnable «∑Ò±ª÷ÿ–¥
+	HashMap<String, Double> playerInfo = new HashMap<String, Double>();
+	HashMap<String, Double>[] levelInfo;
+	Gson gson = new Gson();
+	private static Economy economy = null;
+	
+	@Override
     public void onEnable() {
-		getLogger().info("onEnable has been invoked!");
 		getServer().getPluginManager().registerEvents(this, this);
-		
-        // ≤Âº˛‘ÿ»Î ±“™÷¥––µƒ¥˙¬Î£®¬‘£©
+		if (!setupEconomy() ) {
+			getLogger().info(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+		load();
+    }
+	
+	@Override
+    public void onDisable() {
+		save();
     }
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-	    if (cmd.getName().equalsIgnoreCase("ore")) { // »Áπ˚ÕÊº“ ‰»Î¡À/basic‘Ú÷¥––»Áœ¬ƒ⁄»›...
-	    	Block b = getServer().getWorld("Bskyblock_world").getBlockAt(802, 85, 0);
-	    	getLogger().info(b.toString());
-	    	getLogger().info(b.getType().toString());
-	        getLogger().info(b.getLocation().toString());
-	    	
-	        return true;
-	    } //»Áπ˚“‘…œƒ⁄»›≥…π¶÷¥––£¨‘Ú∑µªÿtrue 
-	    // »Áπ˚÷¥–– ß∞‹£¨‘Ú∑µªÿfalse.
+	    if (cmd.getName().equalsIgnoreCase("ore")) {
+	    	if (args.length == 0) {
+	    		
+	    	}else if (args.length == 1) {
+	    		if(args[0].equalsIgnoreCase("level")) {
+	    			double level = playerInfo.get(sender.getName());
+	    			sender.sendMessage("Your Ore Generator is in level " + (int)level);
+
+	    			return true;
+	    		}else if (args[0].equalsIgnoreCase("upgrade")) {
+	    			OreGenLevelUp((Player)sender);
+	    			return true;
+	    		}else {
+	    			
+	    		}
+	    		
+	    	}else {
+	    		sender.sendMessage("Too many arguments!");
+	    	}
+	    }
+	    
 	    return false;
 	}
 	
     @EventHandler
     public void onStoneGen(BlockFormEvent event) {
-    	new OreGen(this, event.getBlock());
-    	BukkitTask task = new OreGen(this, event.getBlock()).runTaskLater(this, 1);
+    	//
+    	new OreGen(event.getBlock(), levelInfo[0]);
+    	BukkitTask task = new OreGen(event.getBlock(), levelInfo[0]).runTaskLater(this, 1);
+    }
+    
+    @EventHandler
+    public void onPlayerLogin(PlayerJoinEvent event) {
+    	String playerName = event.getPlayer().getName();
+    	
+    	if(!playerInfo.containsKey(playerName)) {
+    		getLogger().info("Sign up new player: " + playerName);
+    		playerInfo.put(playerName, 0.0);
+    	}
+    }
+    
+    @SuppressWarnings("unchecked")
+	private void load() {
+    	try {
+    		File file = new File("plugins/OreGenUltimateX");
+    		if(!file.exists()){//Â¶ÇÊûúÊñá‰ª∂Â§π‰∏çÂ≠òÂú®
+    			file.mkdir();//ÂàõÂª∫Êñá‰ª∂Â§π
+    		}
+    		
+    		//playerinfo
+    		file = new File("plugins/OreGenUltimateX/playerinfo.txt");
+    		if (!file.exists()) {
+    			return;
+    		}
+    		
+    		FileInputStream fis=new FileInputStream("plugins/OreGenUltimateX/playerinfo.txt");
+            BufferedInputStream bis=new BufferedInputStream(fis);
+            String json = "";
+            byte[] buffer = new byte[1024];
+            int len = 0;
+            
+            while((len=bis.read(buffer))!=-1){
+            	json+=new String(buffer, 0, len);
+            }
+            
+            bis.close();
+            
+            //playerInfo = JSON.parseObject(json, HashMap.class);
+            playerInfo = gson.fromJson(json, HashMap.class);
+            
+            //levelinfo
+            file = new File("plugins/OreGenUltimateX/levelinfo.txt");
+    		if (!file.exists()) {
+    			getLogger().info("Create default levelinfo");
+    			levelInfo = new HashMap[1];
+    			levelInfo[0] = new HashMap<String, Double>();
+    			levelInfo[0].put("need", 100.0);
+    			levelInfo[0].put("coal", 3.0);
+    			
+    			json = gson.toJson(levelInfo);
+    			
+    			FileOutputStream fos=new FileOutputStream("plugins/OreGenUltimateX/levelinfo.txt");
+                BufferedOutputStream bos=new BufferedOutputStream(fos);
+                
+                bos.write(json.getBytes(),0,json.getBytes().length);
+                bos.flush();
+                bos.close();
+    		}
+    		
+    		fis=new FileInputStream("plugins/OreGenUltimateX/levelinfo.txt");
+            bis=new BufferedInputStream(fis);
+            json = "";
+            len = 0;
+            
+            while((len=bis.read(buffer))!=-1){
+            	json+=new String(buffer, 0, len);
+            }
+            
+            bis.close();
+            
+            levelInfo = gson.fromJson(json, HashMap[].class);
+
+    	}catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    }
+    
+    private void save() {
+    	try {
+    		//String json = JSON.toJSONString(playerInfo);
+    		String json = gson.toJson(playerInfo);
+    		
+    		File file = new File("plugins/OreGenUltimateX");
+    		if(!file.exists()){//Â¶ÇÊûúÊñá‰ª∂Â§π‰∏çÂ≠òÂú®
+    			file.mkdir();//ÂàõÂª∫Êñá‰ª∂Â§π
+    		}
+    		
+    		FileOutputStream fos=new FileOutputStream("plugins/OreGenUltimateX/playerinfo.txt");
+            BufferedOutputStream bos=new BufferedOutputStream(fos);
+            
+            bos.write(json.getBytes(),0,json.getBytes().length);
+            bos.flush();
+            bos.close();
+            
+    	}catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    }
+    
+    private void OreGenLevelUp(Player player) {
+    	String playerName = player.getName();
+    	double dlevel = playerInfo.get(playerName);
+    	int level = (int)dlevel;
+
+    	
+    	if(levelInfo == null || levelInfo.length <= level) {
+    		player.sendMessage("" + levelInfo.length);
+    		player.sendMessage("There is no higher level");
+    		return;
+    	}
+
+    	if(economy.getBalance(player) < levelInfo[level].get("need")) {
+    		player.sendMessage("You don't have enough money.");
+    		player.sendMessage(levelInfo[level].get("need").toString() + " is needed");
+    		player.sendMessage("You have " + economy.getBalance(player));
+    		return;
+    	}
+
+    	economy.withdrawPlayer(player, levelInfo[level].get("need"));
+    	playerInfo.replace(playerName, level + 1.0);
+    	player.sendMessage("Your Ore Generator level up. It's in level " + (level + 1));
+    }
+    
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        economy = rsp.getProvider();
+        return economy != null;
     }
 }
